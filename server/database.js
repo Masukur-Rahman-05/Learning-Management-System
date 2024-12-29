3c641a2be1798f34fc49bd3f7da64d72f5c91275
Masukur Rahman <blackhat112001@gmail.com>
2024-12-28 21:04:19 -0800

    Wrap the MONGO_URL in environment variable

--- server/database.js
+++ server/database.js
@@ -1,12 +1,11 @@
-
+import dotenv from "dotenv";
+dotenv.config();
 
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://masukrahoman5:masukrahoman6@lmscluster.vuxmp.mongodb.net/LMS"
-    );
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("Database Connected");
   } catch (error) {
     console.log(error.message);
