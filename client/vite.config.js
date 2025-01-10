d69733b45a7b2009d06cfefd7c8ae9235684f4d6
Masukur Rahman <blackhat112001@gmail.com>
2025-01-10 00:50:04 -0800

    Fix the issue

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,22 +1,8 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
-// import path from "path"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from "path"
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-// })
-
-
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -24,37 +10,51 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    outDir: "dist",
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: [
-            "react",
-            "react-dom",
-            "react-router-dom",
-            "@reduxjs/toolkit",
-            "react-redux",
-          ],
-          ui: [
-            "@radix-ui/react-accordion",
-            "@radix-ui/react-checkbox",
-            "@radix-ui/react-dropdown-menu",
-            "@radix-ui/react-label",
-            "@radix-ui/react-progress",
-            "@radix-ui/react-select",
-            "@radix-ui/react-separator",
-            "@radix-ui/react-slot",
-            "@radix-ui/react-switch",
-            "@radix-ui/react-tabs",
-            "@radix-ui/react-toast",
-          ],
-        },
-      },
-    },
-  },
-});
+})
+
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react-swc";
+// import path from "path";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+//   build: {
+//     outDir: "dist",
+//     sourcemap: true,
+//     rollupOptions: {
+//       output: {
+//         manualChunks: {
+//           vendor: [
+//             "react",
+//             "react-dom",
+//             "react-router-dom",
+//             "@reduxjs/toolkit",
+//             "react-redux",
+//           ],
+//           ui: [
+//             "@radix-ui/react-accordion",
+//             "@radix-ui/react-checkbox",
+//             "@radix-ui/react-dropdown-menu",
+//             "@radix-ui/react-label",
+//             "@radix-ui/react-progress",
+//             "@radix-ui/react-select",
+//             "@radix-ui/react-separator",
+//             "@radix-ui/react-slot",
+//             "@radix-ui/react-switch",
+//             "@radix-ui/react-tabs",
+//             "@radix-ui/react-toast",
+//           ],
+//         },
+//       },
+//     },
+//   },
+// });
 
 
 
