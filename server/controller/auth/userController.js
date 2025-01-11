6222fdcaa2b0d12e63a91791600447b74502aca4
Masukur Rahman <blackhat112001@gmail.com>
2025-01-11 05:54:04 -0800

    Fix the auth issue

--- server/controller/auth/userController.js
+++ server/controller/auth/userController.js
@@ -82,7 +82,7 @@ export const loginUser = async (req, res) => {
     return res
       .cookie("token", token, {
         httpOnly: true,
-        secure: false,
+        secure: true,
         sameSite: "Lax",
       })
       .json({
